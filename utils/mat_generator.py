6801d18c03be32f236f09030743c9cea38ebbe52
juyingnan <bunny_gg@hotmail.com>
2022-07-06 19:40:27 -0700

    optimized the memory usage

--- utils/mat_generator.py
+++ utils/mat_generator.py
@@ -46,20 +46,14 @@ layer_nrow_dict = {
     'final0': 8,
     'final1': 8,
     'final2': 1,
+    'raw': 4,
 }
 
 
-def read_img_files(path, batch_size, filter):
-    """ read images from folder """
-    print('reading the images:%s' % path)
-
-    # add a temp filter for saving space
-    file_name_list = [file_name for file_name in os.listdir(path)
-                      if (os.path.isfile(os.path.join(path, file_name)) and filter in file_name)]
-    mats = {}
-    infos = {}
-
+def read_img_files(path, file_name_list, batch_size):
     idx = 0
+    mat = []
+    info = []
 
     for file_name in file_name_list:
         file_path = os.path.join(path, file_name)
@@ -69,10 +63,6 @@ def read_img_files(path, batch_size, filter):
         # read img metafile
         info_list = file_name.split('.')[0].split('_')
         meta_info_labels = []
-
-        # skip the raw image for now
-        if len(info_list) < 3:
-            continue
 
         # -- image_id
         image_id = int(info_list[0].split('image')[1])
@@ -89,8 +79,8 @@ def read_img_files(path, batch_size, filter):
         # print("-- patch_id", patch_id)
         meta_info_labels.append(patch_id)
 
-        id = batch_id * batch_size + patch_id
-        meta_info_labels.append(id)
+        _id = batch_id * batch_size + patch_id
+        meta_info_labels.append(_id)
         # print("-- id", id)
 
         meta_info_labels.append(idx)
@@ -127,12 +117,6 @@ def read_img_files(path, batch_size, filter):
         meta_info_labels.append(patch_w)
         meta_info_labels.append(patch_h)
 
-        if layer_name not in mats:
-            mats[layer_name] = []
-
-        if layer_name not in infos:
-            infos[layer_name] = []
-
         # img -> mat
         patches = []
         for c in range(n_col):
@@ -145,10 +129,59 @@ def read_img_files(path, batch_size, filter):
         #     io.imsave(f"{idd}.jpg", p)
         #     idd += 1
 
-        mats[layer_name].append(np.stack(patches, axis=0))
-        infos[layer_name].append(meta_info_labels)
+        mat.append(np.stack(patches, axis=0))
+        info.append(meta_info_labels)
 
-    return mats, infos
+    return mat, info
+
+
+def read_raw_img_files(path, file_name_list, batch_size):
+    idx = 0
+    mat = []
+    info = []
+
+    for file_name in file_name_list:
+        file_path = os.path.join(path, file_name)
+        img = io.imread(file_path)
+        # io.imsave(file_path, img)
+
+        # read img metafile
+        info_list = file_name.split('.')[0].split('_')
+
+        image_id = int(info_list[0].split('image')[1])
+        batch_id = int(info_list[1].split('batch')[1])
+        layer_name = 'raw'
+
+        n_row = layer_nrow_dict[layer_name]
+        img_w = img.shape[1]
+        img_h = img.shape[0]
+        patch_w = patch_h = img_w // n_row
+        n_col = img_h // patch_h
+
+        layer_count = n_row * n_col
+
+        # img -> mat
+        patch_id = 0
+        for c in range(n_col):
+            for r in range(n_row):
+                patch = img[c * patch_h: c * patch_h + patch_h, r * patch_w: r * patch_w + patch_w]
+
+                _id = batch_id * batch_size + patch_id
+                model_id = "none"
+
+                meta_info_labels = [image_id, batch_id, patch_id,
+                                    _id, idx,
+                                    model_id, layer_name, layer_count, patch_w, patch_h]
+
+                # save test
+                # io.imsave(f"{image_id}_{batch_id}_{patch_id}_{_id}_{idx}.jpg", patch)
+
+                mat.append(patch)
+                info.append(meta_info_labels)
+                patch_id += 1
+                idx += 1
+        del img
+    return mat, info
 
 
 if __name__ == '__main__':
@@ -156,18 +189,36 @@ if __name__ == '__main__':
     root_path = r'X:\temp\\'
     raw_file_path = root_path + organ_item
     np.seterr(all='ignore')
-    raw_mats, meta_info_labels = read_img_files(raw_file_path, batch_size=12, filter="upsample1")
 
-    for l_name in raw_mats:
-        sio.savemat(root_path + f'{organ_item}_{l_name}.mat', mdict={'feature_matrix': raw_mats[l_name],
-                                                                     'image_id': [item[0] for item in meta_info_labels[l_name]],
-                                                                     'batch_id': [item[1] for item in meta_info_labels[l_name]],
-                                                                     'patch_id': [item[2] for item in meta_info_labels[l_name]],
-                                                                     'id': [item[3] for item in meta_info_labels[l_name]],
-                                                                     'idx': [item[4] for item in meta_info_labels[l_name]],
-                                                                     'model_id': [item[5] for item in meta_info_labels[l_name]],
-                                                                     'layer_name': [item[6] for item in meta_info_labels[l_name]],
-                                                                     'layer_count': [item[7] for item in meta_info_labels[l_name]],
-                                                                     'width': [item[8] for item in meta_info_labels[l_name]],
-                                                                     'height': [item[9] for item in meta_info_labels[l_name]],
+    all_file_name_list = [file_name for file_name in os.listdir(raw_file_path)
+                          if (os.path.isfile(os.path.join(raw_file_path, file_name)))]
+    file_name_list_dict = {}
+    for file_name in all_file_name_list:
+        file_name_info = file_name.split('_')
+        if len(file_name_info) > 2:
+            layer = file_name_info[3]
+        else:
+            layer = "raw"
+        if layer not in file_name_list_dict:
+            file_name_list_dict[layer] = []
+        file_name_list_dict[layer].append(file_name)
+
+    for l_name in file_name_list_dict:
+        """ read images from folder """
+        print('reading the images:%s\t%s' % (raw_file_path, l_name))
+        if l_name != "raw":
+            raw_mat, meta_info = read_img_files(raw_file_path, file_name_list_dict[l_name], batch_size=12)
+        else:
+            raw_mat, meta_info = read_raw_img_files(raw_file_path, file_name_list_dict[l_name], batch_size=12)
+        sio.savemat(root_path + f'{organ_item}_{l_name}.mat', mdict={'feature_matrix': raw_mat,
+                                                                     'image_id': [item[0] for item in meta_info],
+                                                                     'batch_id': [item[1] for item in meta_info],
+                                                                     'patch_id': [item[2] for item in meta_info],
+                                                                     'id': [item[3] for item in meta_info],
+                                                                     'idx': [item[4] for item in meta_info],
+                                                                     'model_id': [item[5] for item in meta_info],
+                                                                     'layer_name': [item[6] for item in meta_info],
+                                                                     'layer_count': [item[7] for item in meta_info],
+                                                                     'width': [item[8] for item in meta_info],
+                                                                     'height': [item[9] for item in meta_info],
                                                                      })

75440872a17ba7df67408a4f93027ed03d632b26
juyingnan <bunny_gg@hotmail.com>
2022-07-10 16:15:01 -0700

    optimized the codes and added save function

--- visualizations/visualization_test.py
+++ visualizations/visualization_test.py
@@ -5,9 +5,9 @@ from matplotlib import gridspec
 # from MulticoreTSNE import MulticoreTSNE as TSNE
 from sklearn.manifold import TSNE
 from scipy import io
-import numpy as np
 import math
 import os
+from utils import tools
 
 root_path = r'X:\temp/'
 file_name_list = [file_name for file_name in os.listdir(root_path) if file_name.endswith(".mat")]
@@ -21,17 +21,9 @@ rows = int(math.ceil(N / cols))
 gs = gridspec.GridSpec(rows, cols)
 
 # get FTU and non-FTU label
-non_zero_labels = []
 final_mat = io.loadmat(root_path + 'colon_final2.mat')
 final_output = final_mat.get('feature_matrix')
-for mask in final_output:
-    non_zero_rate = len(np.nonzero(mask)[0]) / (mask.shape[1] * mask.shape[2])
-    # print(non_zero_rate)
-    if non_zero_rate > 0.05:
-        non_zero_labels.append(1)
-    else:
-        non_zero_labels.append(0)
-
+non_zero_labels = tools.get_is_ftu_label(final_output)
 
 for t in range(len(file_name_list)):
     # for t in range(3):
@@ -39,10 +31,10 @@ for t in range(len(file_name_list)):
     mat_path = root_path + file_name
     digits = io.loadmat(mat_path)
 
-    X, y = digits.get('feature_matrix'), digits.get('image_id')[0]  # X: nxm: n=500//sample, m=12,10,71,400//feature
+    X = digits.get('feature_matrix')
+    # y = digits.get('image_id')[0]
     y = non_zero_labels
     print(X.shape)
-    # X = X[:, :, t:t + 1]
     X = X.reshape(X.shape[0], -1)
     n_samples, n_features = X.shape
 
@@ -53,13 +45,16 @@ for t in range(len(file_name_list)):
           "After {} iter: Org data dimension is {}. Embedded data dimension is {}".format(tsne.n_iter, X.shape[-1],
                                                                                           X_tsne.shape[-1]))
 
-    x_min, x_max = X_tsne.min(0), X_tsne.max(0)
-    X_norm = (X_tsne - x_min) / (x_max - x_min)
+    # save tsne raw result to mat
+    digits['feature_matrix'] = X_tsne
+    io.savemat(root_path + rf'colon_tsne\{file_name.replace(".", "_tsne.")}', mdict=digits)
+
+    X_norm = tools.get_norm(X_tsne)
     ax = fig.add_subplot(gs[t])
     # plt.figure(figsize=(8, 8))
     for i in range(X_norm.shape[0]):
         # plt.plot(X_norm[i, 0], X_norm[i, 1], '.', color=plt.cm.Set1(int(y[i][0])))
-        ax.text(X_norm[i, 0], X_norm[i, 1], str(y[i]), color=plt.cm.tab20(int(y[i])),
+        ax.text(X_norm[i, 0], X_norm[i, 1], str(y[i]), color=plt.cm.tab20(int(y[i] * 2)),  # *2 has color contrast+
                 fontdict={'size': 8})
         ax.title.set_text(file_name.split('_')[1].split('.')[0])
         # ax.xticks([])
